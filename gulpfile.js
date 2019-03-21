141693acf7a7105612c786e5ecad56d0de192fe5
Nico Watine <nico@studioncreations.com>
2019-03-21 08:36:49 -0500

    Run gulp with tweaked html string

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var uncss = require('gulp-uncss');
 gulp.task('default', function () {
 	return gulp.src('css/cdplants.css')
 		.pipe(uncss({
-			html: ['index.html', 'FamilyBotanical/*.html', 'genera/*.html', 'height/*.html', 'light/*.html', 'plants/*.html', 'temperature/*.html']
+			html: ['index.html', '**/*.html']
 		}))
 		.pipe(gulp.dest('./css/opt'));
 });
